3aa1be4b5f721f8936098d11fbef434195a06774
Zac Colley <zac@clubpenguin.email>
2021-09-10 18:56:24 +0100

    dont hide dotenv require

--- netlify/functions/twitch-user/twitch-user.js
+++ netlify/functions/twitch-user/twitch-user.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 
 const fetch = (...args) => {
   return import("node-fetch").then(({ default: fetch }) => fetch(...args));
